1876cabb1c9d84a721a2872bd1d52395fbb637b3
Nancy <nancy62924@gmail.com>
2022-04-22 20:40:28 +0800

    add app and die component

---
+++ src/Die.js
@@ -0,0 +1,18 @@
+function Die({ value }) {
+  return (
+    <section className="die-wrapper">
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+      <div>{value}</div>
+    </section>
+  );
+}
+
+export default Die;
